6d6f4018b11aa354c362401e34f811cc612a83c9
FaturPohontu <faturpohontu954@gmail.com>
2024-12-23 07:15:15 +0800

    up db

--- pesanmakanan.sql
+++ pesanmakanan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2024 at 11:15 AM
+-- Generation Time: Dec 23, 2024 at 12:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,6 +42,43 @@ CREATE TABLE `db_cust` (
 INSERT INTO `db_cust` (`id`, `nama`, `telp`, `email`, `pass`) VALUES
 (7, 'Fatur', '123', 'faturpohontu954@gmail.com', '$2y$10$O3IQfEE9NyBsgRvwVh9zXOl8T9Jv1ALw0n7dU5a5TfiotYbOE9fMa');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `harga` int(100) NOT NULL,
+  `Deskripsi` text NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id`, `nama`, `harga`, `Deskripsi`, `gambar`) VALUES
+(1, 'Rice Bowl Nuget', 15000, 'Makanan Paling enak sedunia', 'rb.jpeg'),
+(2, 'Rice Bowl tempe', 10000, 'Makanan Paling enak', 'rbb.jpeg'),
+(3, 'Rice Bowl Telur', 12000, 'Makanan Paling enak', 'rb.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_trx` int(11) NOT NULL,
+  `barang` int(11) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +90,19 @@ ALTER TABLE `db_cust`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_trx`),
+  ADD KEY `barang` (`barang`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +111,28 @@ ALTER TABLE `db_cust`
 --
 ALTER TABLE `db_cust`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_trx` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`barang`) REFERENCES `tb_barang` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
